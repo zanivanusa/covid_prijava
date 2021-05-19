14eb73e8be3ef299ece3430fa7283617271f0750
zanivanusa <60394411+zanivanusa@users.noreply.github.com>
2021-05-20 00:00:21 +0200

    Delete prijava directory

--- prijava/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.prijava, name='prijava'),
-    path('podatki/', views.prijava_podatki, name='podatki'),
-
-]
