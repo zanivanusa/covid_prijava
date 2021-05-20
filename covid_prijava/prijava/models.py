627b5e8bf08193b5007e791bd910af76eeb96e0f
zanivanusa <60394411+zanivanusa@users.noreply.github.com>
2021-05-20 19:16:57 +0200

    popravil emso in url

--- covid_prijava/prijava/models.py
+++ covid_prijava/prijava/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 import re
+
 def is_letter(string):
     if string.isdigit():
         raise ValidationError("niste vnesli crke")
@@ -15,12 +16,23 @@ def emso_regex(emso):
     else:
         raise ValidationError("Emšo ni validen")
 
+def emso_verify(emso):
+
+    emso_factor_map = [7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+    control_digit = sum([int(emso[i]) * emso_factor_map[i] for i in range(12)]) % 11
+    control_digit = 0 if control_digit == 0 else 11 - control_digit
+    if control_digit == int(emso[12]):
+        return emso
+    else:
+        raise ValidationError("Emšo ni validen")
+
 
 class Prijava(models.Model):
 
     ime = models.CharField(max_length = 1, validators=[is_letter])
     priimek = models.CharField(max_length = 1, validators=[is_letter])#ce se emso zacne s 0, zacetek zbrise ker je int
-    emso = models.BigIntegerField(unique=True, validators=[emso_regex])
+    emso = models.CharField(unique=True, validators=[emso_regex, emso_verify], max_length=13)
     mail = models.EmailField(max_length = 60)
     datum_vpisa = models.DateTimeField(auto_now_add =True)
-    
+
+
